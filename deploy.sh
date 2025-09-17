3fab21be666ea5d05fb4d78deb6a3402ddcee58b
Musab74 <Rmcbaxr@gmail.com>
2025-09-17 16:36:30 +0900

    fix:upload .env

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,10 @@ git reset --hard
 git checkout master
 git pull origin master
 
-docker compose up -d 
+npm install --legacy-peer-deps
+npm run build
 
+# 4) Restart PM2 from the SAME folder so the app sees .env in its CWD
+pm2 delete live-backend
+pm2 start dist/main.js --name live-backend
+pm2 save
