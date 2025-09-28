182a51889cce4382e05335ed151ed04fc3d43ce8
Musab74 <Rmcbaxr@gmail.com>
2025-09-29 08:38:51 +0900

    fix:getMeetings logic changed

--- src/components/auth/auth.module.ts
+++ src/components/auth/auth.module.ts
@@ -7,7 +7,7 @@ import { JwtModule } from '@nestjs/jwt';
   imports: [
     HttpModule,
     JwtModule.register({
-      secret: `${process.env.SECRET_TOKEN}`,
+      secret: `${process.env.JWT_SECRET}`,
       signOptions: { expiresIn: '3h' },
     }),
   ],
